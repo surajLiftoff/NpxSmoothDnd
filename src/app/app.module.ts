695dfadc7b798003ba83de85f86de89a9b4db4f4
unknown <suraj@liftoffllc.com>
2022-05-30 23:20:15 +0530

    Working on Angular 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxSmoothDnDModule } from 'ngx-smooth-dnd';
+import * as pages from '../pages';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    pages.CardsComponent,
+    pages.CopyComponent,
+    pages.DragClassComponent,
+    pages.DragDelayComponent,
+    pages.DragHandleComponent,
+    pages.FormComponent,
+    pages.GroupsComponent,
+    pages.LockAxisComponent,
+    pages.NestedComponent,
+    pages.SimpleComponent,
+    pages.SimpleHorizontalComponent,
+    pages.SimpleScrollerComponent,
+    pages.TransitionDurationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSmoothDnDModule
   ],
   providers: [],
   bootstrap: [AppComponent]
